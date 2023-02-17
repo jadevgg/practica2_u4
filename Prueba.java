e0651e2f5ef09bb4d1b372d0e383f2f96e136067
jadevgg <124889409+jadevgg@users.noreply.github.com>
2023-02-17 20:18:20 +0100

    Create Prueba.java

---
+++ Prueba.java
@@ -0,0 +1,50 @@
+package practica2_u4;
+
+public class Prueba {
+	
+	private int id;
+	private String nombre;
+	private String apellido;
+	private int telefono;
+
+	public Prueba(int id, String nombre, String apellido, int telefono) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.telefono = telefono;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+}
